ad6402946c1bac885a380b2efbc0952d917e7721
kvadrats <emils.kantans@gmail.com>
2016-02-03 09:43:15 -0800

    validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
 	fixtures :products
-end
+
 	test "product attributes must not be empty" do 
 	product = Product.new
 	assert product.invalid?
 	assert product.errors[:title].any?
 	assert product.errors[:description].any?
 	assert product.errors[:price].any?
-	assert product.errors[:image_url].any?	
+	assert product.errors[:image_url].any?
 	end
 
 	test "product price must be possitive with -1" do 
@@ -60,4 +60,12 @@ end
 			assert new_product(name).invalid?, "#{name} shouldn't be valid"
 		end
 	end
+
+	test "product is not valid without a nuique title" do 
+		product = Product.new(title: products(:ruby).title,
+							  description: "yyy",
+							  price: 1,
+							  image_url: "fred.gif")
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
 end
