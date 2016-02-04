a1f32c4a581b076f0c466b5138a39ff3f4fce28b
kvadrats <emils.kantans@gmail.com>
2016-02-03 17:39:14 -0800

    cart with stacking products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
-	def self.latest
-		Product.order('updated_at').last
-	end
+
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 validates :title, :description, :image_url, presence: true	
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -10,4 +11,19 @@ validates :image_url, format: {
 		  with: %r{\.(gif|jpg|png|jpeg|)\Z}i,
 		  message: 'must be an URL for gif, jpg, png or jpeg image!'
 }
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
+	
+	def self.latest
+		Product.order('updated_at').last
+	end
 end
