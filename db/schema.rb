e8da0776df5b7fbb7bfecb3dbf5e6057e1ba573c
kvadrats <emils.kantans@gmail.com>
2016-02-08 17:34:33 -0800

    unfinished login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205213702) do
+ActiveRecord::Schema.define(version: 20160209001143) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20160205213702) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
